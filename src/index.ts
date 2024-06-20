4e4c69a924a9a7e5f9ca94b5c8a30988cc75e257
Skjaldbaka17 <skjaldbaka17@gmail.com>
2024-06-20 22:39:42 +0100

    rm user gh token

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("HELLO WORLD!");
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit } from "@actions/github";
+
+export async function run() {
+  const token = getInput("gh-token");
+  const label = getInput("label");
+
+  const octokit = getOctokit(token);
+  const pullRequest = context.payload.pull_request;
+
+  try {
+    if (!pullRequest) {
+      throw new Error("This action can only be run on Pull Requests");
+    }
+
+    await octokit.rest.issues.createComment({
+      //   owner: context.repo.owner,
+      //   repo: context.repo.repo,
+      //   issue_number: pullRequest.number,
+      //   labels: [label],
+      body: "Testing this new thing",
+      issue_number: pullRequest.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+    });
+  } catch (error) {
+    setFailed((error as Error)?.message ?? "Unknown error");
+  }
+}
+
+run();
