7e740b1bf63f1840430cc6d01b9fc766431de7a0
Skjaldbaka17 <skjaldbaka17@gmail.com>
2024-06-21 00:07:19 +0100

    added waves youtube link

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,32 @@ import { getInput, setFailed } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 
 const generateRandomSentence = (): string => {
+  const wavesYT = [
+    "https://youtu.be/pUjE9H8QlA4?si=FqKSxR6SOnH7orih",
+    "https://youtu.be/bn9F19Hi1Lk?si=gW1dc24YX-H5KFA1",
+    "https://youtu.be/vPhg6sc1Mk4?si=_7BMILSIwqQppM1Y",
+    "https://youtu.be/dKlgCk3IGBg?si=aWHu9oZ64l1y9dhO",
+    "https://youtu.be/nydwk87iEuM?si=22EhIOoCv7gGnDw3",
+    "https://youtu.be/0a5WyAjL1MM?si=1dPtsJ8DHF8nW8m4",
+    "https://youtu.be/zZqxPozl2Ec?si=BAm5JCxQUtGv4aaD",
+    "https://youtu.be/JkMWoHjnaRw?si=Rakn8u285i3UGg5-",
+    "https://youtu.be/mRD0-GxqHVo?si=IiBNuPF3iLqWvTzV",
+    "https://youtu.be/UIevTTLB_3Q?si=fliBUG8Uz-miLhjr",
+    "https://youtu.be/bjOGNVH3D4Y?si=bXwVFdq6eH_-CcoL",
+    "https://youtu.be/o-llwv4pLrg?si=N8KNaJqypZxAxsql",
+    "https://youtu.be/x-x-eyZf8Ko?si=_2DUfrv7tQnN_wCU",
+    "https://youtu.be/6vVa6jiDQF8?si=lWBWxQEaoLyRXpKY",
+    "https://youtu.be/ckWRHc85yBU?si=YVitxrhEdbKeFgFF",
+    "https://youtu.be/vq24zYNUTpE?si=GDXX2aMfgK_YJ21l",
+    "https://youtu.be/anLxLPzgIG4?si=b9QmYMx-ZOmv5kUQ",
+    "https://youtu.be/JCLPMoT8FRY?si=cdOA4STNJuEJC1zy",
+    "https://youtu.be/eNpqsJM-JLw?si=23NRgFheddn5MwHU",
+    "https://youtu.be/p1iu_U4kYWs?si=tnuK48pmbJNE0OlD",
+    "https://youtu.be/W6TpQ3JcYfE?si=WbrqyGDFOWcIA8BE",
+    "https://youtu.be/p-4GefgMY5c?si=S3CMxKxZN1GfL6Dz",
+    "https://youtu.be/CLk7A7HXhYQ?si=nhWqeEq8hcm4WXhZ",
+    "https://youtu.be/t-E9PjjMY-U?si=gxCn7gJWvzWaNGYv",
+  ];
   const emojis = [
     "🌊",
     "🔥",
@@ -19,21 +45,23 @@ const generateRandomSentence = (): string => {
     "🎶",
     "🎨",
   ];
+
+  const randomYoutubeLink = wavesYT[Math.floor(Math.random() * wavesYT.length)];
   const variations = [
-    "This is quite delightful, but a sprinkle more waves would elevate it 🌊",
-    "This is rather charming, yet a splash more waves could really make it pop 🌊",
-    "This is lovely, but a surge of waves would truly amplify its beauty 🌊",
-    "This is nice, yet a ripple of waves would add an enchanting touch 🌊",
-    "This is quite appealing, but a wave of waves would bring it to life 🌊",
-    "This is pretty good, but a tide of waves would bring out its brilliance 🌊",
-    "This is really nice, yet a cascade of waves... imagine the magic 🌊",
-    "This is quite good, though more waves could turn it into a masterpiece 🌊",
-    "This is awesome, but it craves more waves to reach its full potential 🌊",
-    "This is cool, but a swell of waves would set it apart 🌊",
-    "This is amazing, yet yearns for more waves to showcase its splendor 🌊",
-    "This is wonderful, yet a surge of waves, now that's a vision 🌊",
-    "This is splendid, yet a flourish of waves would add a touch of grandeur 🌊",
-    "This is superb, but a crescendo of waves could turn heads 🌊",
+    `This is quite delightful, but a sprinkle more [waves](${randomYoutubeLink}) would elevate it 🌊`,
+    `This is rather charming, yet a splash more [waves](${randomYoutubeLink}) could really make it pop 🌊`,
+    `This is lovely, but a surge of [waves](${randomYoutubeLink}) would truly amplify its beauty 🌊`,
+    `This is nice, yet a ripple of [waves](${randomYoutubeLink}) would add an enchanting touch 🌊`,
+    `This is quite appealing, but a wave of [waves](${randomYoutubeLink}) would bring it to life 🌊`,
+    `This is pretty good, but a tide of [waves](${randomYoutubeLink}) would bring out its brilliance 🌊`,
+    `This is really nice, yet a cascade of [waves](${randomYoutubeLink})... imagine the magic 🌊`,
+    `This is quite good, though more [waves](${randomYoutubeLink}) could turn it into a masterpiece 🌊`,
+    `This is awesome, but it craves more [waves](${randomYoutubeLink}) to reach its full potential 🌊`,
+    `This is cool, but a swell of [waves](${randomYoutubeLink}) would set it apart 🌊`,
+    `This is amazing, yet yearns for more [waves](${randomYoutubeLink}) to showcase its splendor 🌊`,
+    `This is wonderful, yet a surge of [waves](${randomYoutubeLink}), now that's a vision 🌊`,
+    `This is splendid, yet a flourish of [waves](${randomYoutubeLink}) would add a touch of grandeur 🌊`,
+    `This is superb, but a crescendo of [waves](${randomYoutubeLink}) could turn heads 🌊`,
   ];
 
   const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
